c61b5c6bed2430fcff3f8514ae2ea76774744e4c
Mai Xuan Trieu <xtrieu30@gmail.com>
2023-01-30 00:01:33 +0900

    Update appWriteConfig.js

--- auth-app/src/appwrite/appWriteConfig.js
+++ auth-app/src/appwrite/appWriteConfig.js
@@ -1,6 +1,4 @@
 import { Client, Account } from "appwrite";
 const client = new Client();
-client
-  .setEndpoint("http://127.0.0.1/v1")
-  .setProject("639cdef0a5afa8ad7e48");
+client.setEndpoint("http://127.0.0.1/v1").setProject("63d4fc36bf4512b2ff42");
 export const account = new Account(client);
